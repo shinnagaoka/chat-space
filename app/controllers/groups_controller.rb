21053bfe84de1ae6b9c5ff39e6ef41c149bbc124
shinnagaoka <shin.nagaoka45@gmail.com>
2017-12-10 17:37:35 +0900

    finish_group_

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,33 @@
 class GroupsController < ApplicationController
+	def new
+		@group = Group.new
+		# 配列に要素を追加する動き
+		@group.users << current_user
+	end
+
+	def create
+		@group = Group.new(group_params)
+		if @group.save
+			redirect_to root_path, notice: "グループを作成しました"
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@group = Group.find(params[:id])
+
+	end
+
+	def update
+		group = Group.find(params[:id])
+		group.update(group_params)
+		redirect_to root_path, notice: "グループを変更しました。"
+	end
+
+	private
+	def group_params
+		params.require(:group).permit(:name ,{ user_ids: [] })
+	end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+	has_many :members
+	has_many :groups, through: :members
+
+	validates :name, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
